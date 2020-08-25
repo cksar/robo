355169bf3294a1c428aa8dec45324f9b15e19980
cksar <cesarhsouzasoad@gmail.com>
2020-08-25 17:57:58 -0300

    first commit

---
+++ src/test/java/br/com/ndd/cesar/robo/RoboApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ndd.cesar.robo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
